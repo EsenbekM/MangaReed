2667dea3a37b010d42a578e931349900d1c9c0aa
KanHub02 <usmnvk200207@gmail.com>
2023-01-23 18:17:01 +0600

    fuck

--- users/urls.py
+++ users/urls.py
@@ -12,4 +12,6 @@ urlpatterns = [
     path("signup/", RegisterApiView.as_view(), name="register_api"),
     path("signin/", LoginApiView.as_view(), name="login_api"),
     path("logout/", LogoutApiView.as_view(), name="logout_api"),
+    path("profile/<pk>/", UserProfileView.as_view(), name="profile_api"),
+    path("profile/", UsersListApiView.as_view(), name="profile_api"),
 ]
